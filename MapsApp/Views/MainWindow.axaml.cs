f6e0dee4c6ee2ae9547860184c45ac8808d76359
zciwolvo <zciwolvo@gmail.com>
2023-10-11 09:45:43 +0200

    Update

--- MapsApp/Views/MainWindow.axaml.cs
+++ MapsApp/Views/MainWindow.axaml.cs
@@ -1,11 +1,5 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
-using Mapsui;
-using Mapsui.Layers;
-using Mapsui.Styles;
-using Mapsui.UI.Avalonia;
-using Mapsui.Geometries;
 
 
 namespace MapsApp.Views
@@ -14,59 +8,9 @@ namespace MapsApp.Views
     {
         public MainWindow()
         {
-        }
-
-        private PointLatLng start;
-        private PointLatLng end;
-
-        private void OnMapPointerPressed(object sender, PointerPressedEventArgs e)
-        {
-            // Get the clicked point on the map
-            var point = mapControl.ConvertPoint(e.GetPosition(mapControl), mapControl);
-
-            if (start == null)
-            {
-                // Set the start point
-                start = point;
-            }
-            else if (end == null)
-            {
-                // Set the end point
-                end = point;
-
-                // Draw the path between the two points
-                DrawPath(start, end);
-
-                // Reset the start and end points
-                start = null;
-                end = null;
-            }
-        }
-
-        private void DrawPath(PointLatLng start, PointLatLng end)
-        {
-            // Create a new map layer
-            var layer = new Mapsui.Layers.Layer();
-
-            // Create a new line feature
-            var line = new Mapsui.Geometries.LineString(new[] { start, end });
-
-            // Create a new style for the line
-            var style = new Mapsui.Styles.VectorStyle
-            {
-                Line = new Mapsui.Styles.Pen
-                {
-                    Color = Mapsui.Styles.Color.Red,
-                    Width = 2
-                }
-            };
-
-            // Add the line feature to the layer
-            layer.DataSource = new Mapsui.Data.FeatureCollection(new[] { new Mapsui.Data.Feature(line) });
-            layer.Styles.Add(style);
-
-            // Add the layer to the map
-            mapControl.Map.Layers.Add(layer);
+            var mapControl = new Mapsui.UI.Avalonia.MapControl();
+            mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            Content = mapControl;
         }
     }
 }
